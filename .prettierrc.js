9d1d4a9c40840fb9675431afd73692640960a83d
Pedro Del Moral Lopez <pedrod@zillowgroup.com>
2024-06-20 13:52:52 -0400

    Initial Setup

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+/**
+ * @type {import('prettier').Config}
+ */
+export default {
+    singleQuote: true,
+    tabWidth: 4,
+    printWidth: 120,
+    trailingComma: 'es5',
+    jsxBracketSameLine: true,
+    arrowParens: 'avoid',
+};
